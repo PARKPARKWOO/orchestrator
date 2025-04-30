9224c70478346105acd524d9a40873e5f7460d68
PARKPARKWOO <wy9295@naver.com>
2025-05-01 00:51:15 +0900

    feat: Add GitHub Actions workflow for toolkit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.woo.plugin.version-check") version "0.0.8"
 }
 
 group = "org.woo.orchestrator"
