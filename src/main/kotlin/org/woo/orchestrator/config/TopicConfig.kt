e920a6959ac83401cda61a0748e88596c89d6b23
PARKPARKWOO <wy9295@naver.com>
2025-08-03 17:43:33 +0900

    feat: Add topic configuration

--- src/main/kotlin/org/woo/orchestrator/config/TopicConfig.kt
+++ src/main/kotlin/org/woo/orchestrator/config/TopicConfig.kt
@@ -1,6 +1,7 @@
 package org.woo.orchestrator.config
 
 import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.TopicBuilder
@@ -8,7 +9,12 @@ import org.woo.event.api.ApiEventConstants
 
 // TODO: terraform 으로 이관후 삭제
 @Configuration
-class TopicConfig {
+class TopicConfig(
+    @Value("\${topic.incoming.chat.message}")
+    val incomingChatMessage: String,
+    @Value("\${topic.outgoing.chat.save.message}")
+    val saveMessageTopic: String,
+) {
     @Bean
     fun internalApiCallTopic(): NewTopic =
         TopicBuilder
@@ -21,6 +27,22 @@ class TopicConfig {
     fun externalApiCallTopic(): NewTopic =
         TopicBuilder
             .name(ApiEventConstants.EXTERNAL_API_CALL_TOPIC)
+            .partitions(1)
+            .replicas(1)
+            .build()
+
+    @Bean
+    fun messageTopic(): NewTopic =
+        TopicBuilder
+            .name(incomingChatMessage)
+            .partitions(1)
+            .replicas(1)
+            .build()
+
+    @Bean
+    fun outgoingTopic(): NewTopic =
+        TopicBuilder
+            .name(saveMessageTopic)
             .partitions(1)
             .replicas(1)
             .build()
